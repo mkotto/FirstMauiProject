1b5ac617f07385f5bfccfcfa6aa7a897b7bb6d17
mairbek tungaev <dewdar@live.no>
2023-01-10 00:27:25 +0100

    New class library class for dataaccess and unittesting project

---
+++ DataAccess/List/Staff.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.List
+{
+    public class Staff
+    {
+        List<Person> Employees { get; set; }
+        public Staff()
+        {
+            FillUpEmployees();
+        }
+        public void FillUpEmployees() { 
+            Employees.Add(new Person(1, "Frank", "Castle"));
+            Employees.Add(new Person(2, "Peter", "Parker"));
+            Employees.Add(new Person(3, "Bruce", "Wayne"));
+            Employees.Add(new Person(4, "Bruce", "Banner"));
+        }
+    }
+}
---
+++ DataAccess/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace DataAccess.Models
+{
+    public class Person
+    {
+        public Person(int id, string firstname, string lastname)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
