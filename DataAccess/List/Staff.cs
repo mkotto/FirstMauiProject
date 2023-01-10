d59b59659480ea638f308999b77b85a827d0f213
mairbek tungaev <dewdar@live.no>
2023-01-10 01:07:45 +0100

    minor updates. new loading animation. adding shortcut to the new componentt

--- DataAccess/List/Staff.cs
+++ DataAccess/List/Staff.cs
@@ -20,5 +20,13 @@ namespace DataAccess.List
             Employees.Add(new Person(3, "Bruce", "Wayne"));
             Employees.Add(new Person(4, "Bruce", "Banner"));
         }
+        public static List<Person> GetPeople() {
+            List<Person> temp = new List<Person>();
+            temp.Add(new Person(1, "Frank", "Castle"));
+            temp.Add(new Person(2, "Peter", "Parker"));
+            temp.Add(new Person(3, "Bruce", "Wayne"));
+            temp.Add(new Person(4, "Bruce", "Banner"));
+            return temp; 
+        }
     }
 }
